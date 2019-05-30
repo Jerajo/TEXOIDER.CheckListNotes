9ed680bbc58d709841fd745d251ded151221c547
Jesse R. Jose <jessejose@outlook.es>
2019-05-29 20:12:32 -0400

    Modelo InitFile agregado

---
+++ CheckListNotes/CheckListNotes/Models/Interfaces/IInitFile.cs
@@ -0,0 +1,13 @@
+﻿namespace CheckListNotes.Models.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    public interface IInitFile
+    {
+        string LastPageName { get; set; }
+        DateTime LastResetTime { get; set; }
+        List<CheckListTasksModel> ListOfLists { get; set; }
+        CheckListTasksModel LastList { get; set; }
+        CheckTaskModel LastTask { get; set; }
+    }
+}
