e85d359ccbe4c8511523b8f4d5f8cd32b400edd9
Jesse R. Jose <jessejose@outlook.es>
2019-05-29 20:11:45 -0400

    Dependencie service reducido

--- CheckListNotes/CheckListNotes/Models/Interfaces/IDeviceHelper.cs
+++ CheckListNotes/CheckListNotes/Models/Interfaces/IDeviceHelper.cs
@@ -1,22 +1,11 @@
 ﻿namespace CheckListNotes.Models.Interfaces
 {
-    using CheckListNotes.Models;
     using PortableClasses.Interfaces;
-    using System.Collections.Generic;
     public interface IDeviceHelper
     {
         bool IsDesktop { get; }
         void CloseApp();
-        List<string> GetAllLists();
-        List<CheckListTasksModel> GetAllLists(List<string> listOfCheckListNames);
-        CheckListTasksModel Read(string path = null);
-        void Write(object model, string path = null);
-        void Rename(string oldName, string newName);
-        void Delete(string name);
-        string RegisterNotification(IToast toast);
-        string RegisterAlarm(IToast toast);
         void RegisterBackgroundTask(IBackgroundTask task);
-        void UnregisterToast(string toasId);
         void UnregisterBackgroundTask(string taskName);
     }
 }
