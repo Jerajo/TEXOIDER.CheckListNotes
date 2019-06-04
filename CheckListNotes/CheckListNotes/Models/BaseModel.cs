9245bf294123a7d295e25d32603b67719f89ce79
Jesse R. Jose <jessejose@outlook.es>
2019-06-04 08:36:20 -0400

    Base methods added to PageModel Interface

--- CheckListNotes/CheckListNotes/Models/BaseModel.cs
+++ CheckListNotes/CheckListNotes/Models/BaseModel.cs
@@ -5,7 +5,15 @@
 
     public class BaseModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Occurs when an item is added, removed, changed, moved, or the entire list is
+        /// refreshed.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Notify that a value has changed in the collection.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property.</param>
         public void RisePropertyChanged(string propertyName) 
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) 
--- CheckListNotes/CheckListNotes/Models/Interfaces/IPageModel.cs
+++ CheckListNotes/CheckListNotes/Models/Interfaces/IPageModel.cs
@@ -1,8 +1,15 @@
 ﻿namespace CheckListNotes.Models.Interfaces
 {
+    using FreshMvvm;
     using System.Threading.Tasks;
     public interface IPageModel
     {
+        Task<bool> ShowAlert(string title, string message, string accept, string cancel);
+        Task ShowAlert(string title, string message, string cancel);
+        Task PushPageModel<T>(object data) where T : FreshBasePageModel;
+        Task PopPageModel(object data);
+        Task RegisterToast(CheckTaskViewModel task);
+        Task UnregisterToast(string toastId);
         Task RefreshUI();
     }
 }
