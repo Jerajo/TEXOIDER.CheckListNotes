d645e988eda0781f92cf1d3f9b19f0052232ffe1
Jesse R. Jose <jessejose@outlook.es>
2019-06-04 08:34:37 -0400

    Constant data encasulated

---
+++ CheckListNotes/CheckListNotes/Models/DataTemplate/AppCommandsDataTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace CheckListNotes.Models
+{
+    using System.Collections;
+    public class AppCommandsDataTemplate : IEnumerable
+    {
+        public const string CompleteTask = "CompleteTask";
+        public const string ShowTaskDetails = "ShowTaskDetails";
+
+        public IEnumerator GetEnumerator()
+        {
+            yield return CompleteTask;
+            yield return ShowTaskDetails;
+        }
+
+        public static bool Contains(string value)
+        {
+            foreach (var item in (new AppCommandsDataTemplate()))
+                if ((string)item == value) return true;
+            return false;
+        }
+    }
+}
---
+++ CheckListNotes/CheckListNotes/Models/DataTemplate/BackgroundDataTemplate.cs
@@ -0,0 +1,19 @@
+﻿namespace CheckListNotes.Models
+{
+    public class BackgroundDataTemplate
+    {
+        public const string ResetDaylyTasksTimeTriggerName = "ResetDailyTaskTimeTrigger";
+        public const string ResetDaylyTasksSystemTriggerName = "ResetDailyTaskSystemTrigger";
+        public const string ResetDaylyTasksEntryPoint = "CheckListNotes.Tasks.ResetDailyTasks";
+
+        public const string CompleteTaskName = "CompleteTask";
+        public const string ShowTaskDetailsName = "ShowTaskDetails";
+        public const string CompleteTaskEntryPoint = "CheckListNotes.Tasks.CompleteTask";
+
+        public const string SnoozeToastName = "SnoozeToast";
+        public const string SnoozeToastEntryPoint = "CheckListNotes.Tasks.SnoozeToast";
+
+        //public const string BackgroundTasksGroupName = "CheckListNotesTasks";
+        //public const string BackgroundTasksGroupId = "890fd9-342u3-42342-f4sg53-dh4s5hgf";
+    }
+}
