4a5aad379cce6a6ddbccc76e8a49b09d08769418
Jesse R. Jose <jessejose@outlook.es>
2019-06-05 01:28:41 -0400

    app data template unified

--- CheckListNotes/CheckListNotes/Models/DataTemplate/AppDataTemplate.cs
+++ CheckListNotes/CheckListNotes/Models/DataTemplate/AppDataTemplate.cs
@@ -1,5 +1,31 @@
 ﻿namespace CheckListNotes.Models
 {
+    using System.Collections;
+    public class AppCommandsDataTemplate : IEnumerable
+    {
+        public const string CompleteTask = "CompleteTask";
+        public const string ShowTaskDetails = "ShowTaskDetails";
+
+        public IEnumerator GetEnumerator()
+        {
+            yield return CompleteTask;
+            yield return ShowTaskDetails;
+        }
+
+        public static bool Contains(string value)
+        {
+            foreach (var item in (new AppCommandsDataTemplate()))
+                if ((string)item == value) return true;
+            return false;
+        }
+    }
+
+    public class ToastDataTemplate
+    {
+        public const string AlarmToastArguments = "command=CompleteTask&taskId=";
+        public const string NotificationToastArguments = "command=ShowTaskDetails&taskId=";
+    }
+
     public class BackgroundDataTemplate
     {
         public const string ResetDaylyTasksTimeTriggerName = "ResetDailyTaskTimeTrigger";
