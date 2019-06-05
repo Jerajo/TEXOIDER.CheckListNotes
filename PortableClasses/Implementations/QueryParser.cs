8358fa7e315b8a6880ecff8a24f63db21aada6c3
Jesse R. Jose <jessejose@outlook.es>
2019-06-05 01:35:43 -0400

    QueryParser implementation on PCL

---
+++ PortableClasses/Implementations/QueryParser.cs
@@ -0,0 +1,18 @@
+﻿namespace PortableClasses.Implementations
+{
+    using System.Collections.Generic;
+    public static class QueryParser
+    {
+        public static Dictionary<string, string> Parse(this string args)
+        {
+            var arguments = new Dictionary<string, string>();
+            var queries = args.Split('&');
+            foreach (var query in queries)
+            {
+                var argument = query.Split('=');
+                arguments[argument[0]] = argument[1];
+            }
+            return arguments;
+        }
+    }
+}
