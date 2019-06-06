4e511723b7bfe7e4a286ef59dc66b03b5bb1de9c
Jesse R. Jose <jessejose@outlook.es>
2019-06-06 19:26:23 -0400

    App resources listener created

---
+++ CheckListNotes/CheckListNotes/AppResourcesLisener.cs
@@ -0,0 +1,38 @@
+﻿namespace CheckListNotes
+{
+    using Xamarin.Forms;
+    using CheckListNotes.Models;
+    public class AppResourcesLisener : BaseModel
+    {
+        #region Intance
+
+        private static AppResourcesLisener current;
+        public static AppResourcesLisener Current 
+        {
+            get => current ?? (current = new AppResourcesLisener());
+        }
+
+        #endregion
+
+        #region GETTERS AND SETTERS
+
+        public const string Language = "Language";
+        public const string Theme = "Theme";
+
+        public ResourceDictionary Resources
+        {
+            get => Application.Current.Resources;
+        }
+
+        #endregion
+
+        #region Dispose
+
+        public void Dispose()
+        {
+            current = null;
+        }
+
+        #endregion
+    }
+}
