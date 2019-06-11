7747be9f63bf04204862d0e412f3f074bb7123b5
Jesse R. Jose <jessejose@outlook.es>
2019-06-10 20:10:36 -0400

    Show alerst simplified

--- CheckListNotes/CheckListNotes/Models/Interfaces/IPageModel.cs
+++ CheckListNotes/CheckListNotes/Models/Interfaces/IPageModel.cs
@@ -5,8 +5,8 @@
     public interface IPageModel
     {
         void Init(object initData);
-        Task<bool> ShowAlert(string title, string message, string accept, string cancel);
-        Task ShowAlert(string title, string message, string cancel);
+        Task<bool> ShowAlertQuestion(string title, string message);
+        Task ShowAlertError(string message);
         Task PushPageModel<T>(object data) where T : FreshBasePageModel;
         Task PopPageModel(object data);
         Task RegisterToast(CheckTaskViewModel task);
