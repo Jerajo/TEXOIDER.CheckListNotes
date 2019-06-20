78640802e99e4f906d3d19151caa381e6e1c4718
Jesse R. Jose <jessejose@outlook.es>
2019-06-20 02:49:58 -0400

    SVGImageButton y estenciones para Element completados

---
+++ CheckListNotes/CheckListNotes/Pages/Extensions/ElementExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace CheckListNotes.Pages.Extensions
+{
+    public static class ElementExtensions
+    {
+        public static Rectangle Bounds(this Element value)
+        {
+            if (!(value is VisualElement parent)) return new Rectangle();
+            else return parent.Bounds;
+        }
+
+        public static Rectangle BoundsWithOutPadding(this Element value)
+        {
+            if (!(value is VisualElement parent)) return new Rectangle();
+
+            var padding = value.Padding();
+            var parentBounds = parent.Bounds;
+
+            parentBounds.Width -= padding.HorizontalThickness;
+            parentBounds.Height -= padding.VerticalThickness;
+
+            return parentBounds;
+        }
+
+        public static Thickness Padding(this Element value)
+        {
+            if (!(value is Layout parent)) return new Thickness();
+            else return parent.Padding;
+        }
+    }
+}
--- CheckListNotes/CheckListNotes/Pages/UserControls/SVGImaggeButton.cs
+++ CheckListNotes/CheckListNotes/Pages/UserControls/SVGImaggeButton.cs
@@ -3,10 +3,12 @@ using SkiaSharp;
 using System.IO;
 using System.Text;
 using Xamarin.Forms;
-using Xamarin.Essentials;
 using System.Diagnostics;
 using SkiaSharp.Views.Forms;
+using System.Threading.Tasks;
 using SVG = SkiaSharp.Extended.Svg;
+using CheckListNotes.Pages.Extensions;
+using System.Runtime.CompilerServices;
 
 namespace CheckListNotes.Pages.UserControls
 {
@@ -14,16 +16,33 @@ namespace CheckListNotes.Pages.UserControls
     {
         #region Atributes
 
+#if DEBUG
+        int invalidate = 0, draws = 0, loads = 0;
+#endif
         bool? svgLoaded;
+        bool? svgLoading;
         bool? isDisposing;
-        Color? previousColor;
+        SVG.SKSvg svg;
+        SKBitmap bitmap;
+        SKCanvas canvas;
+        SKColor? previousColor;
+        Rectangle? previousBounds;
+        SKMatrix? scale;
 
         #endregion
 
         public SVGImaggeButton() : base()
         {
             svgLoaded = false;
+            svgLoading = false;
             isDisposing = false;
+            svg = new SVG.SKSvg();
+            previousColor = new SKColor();
+            previousBounds = new Rectangle();
+            bitmap = new SKBitmap();
+            canvas = new SKCanvas(bitmap);
+            scale = SKMatrix.MakeScale(1, 1);
+            this.MeasureInvalidated += OnMeasureInvalidated;
         }
 
         #region Bindable Properties
@@ -55,103 +74,151 @@ namespace CheckListNotes.Pages.UserControls
         private static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var constrol = (SVGImaggeButton)bindable;
-            if (constrol?.Parent == null) return;
-            constrol?.RedrawImage();
+            if (constrol?.Parent == null || 
+                constrol?.Parent.Bounds().IsEmpty == true) return;
+            constrol?.InvalidateMeasure();
         }
 
-        protected override void OnPropertyChanged(string propertyName = null)
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             base.OnPropertyChanged(propertyName);
             if (svgLoaded == true) return;
-            if ((propertyName != nameof(Height) && propertyName != nameof(Width))
-                || this.Parent == null) return;
-            RedrawImage();
+            if (this.Parent == null || this.Parent.Bounds().IsEmpty) return;
+            if (propertyName != nameof(SVGSource) && propertyName != nameof(IconColor) &&
+                propertyName != nameof(Width) && propertyName != nameof(Height)) return;
+#if DEBUG
+            Debug.WriteLine($"Invalidation #{++invalidate}");
+#endif
+            this.InvalidateMeasure();
+        }
+
+        protected virtual void OnMeasureInvalidated(object sender, EventArgs e)
+        {
+            if (svgLoading == true) return;
+            if (this.Parent == null || this.Parent.Bounds().IsEmpty) return;
+            if (previousColor == IconColor?.ToSKColor() && 
+                previousBounds == this.Parent?.Bounds()) return;
+            DrawImage();
         }
 
         #endregion
 
         #region Methods
 
-        private void RedrawImage()
+        protected virtual bool LoadSVG()
         {
-            var parentWidth = (double)this.Parent.GetValue(WidthProperty);
-            var parentHeight = (double)this.Parent.GetValue(HeightProperty);
-            var iconColor = IconColor;
-            var index = SVGSource;
+            var parentBounds = this.Parent.Bounds();
+            if (parentBounds.IsEmpty == true || string.IsNullOrEmpty(SVGSource))
+                return false;
+            if (previousBounds == parentBounds) return true;
 
-            if (parentWidth <= 0 || parentHeight <= 0 ||
-                string.IsNullOrEmpty(index)) return;
-
-            svgLoaded = true;
-            string sFile = $"{FileSystem.AppDataDirectory}/{Path.GetFileNameWithoutExtension(index)}.png";
-
-            // load genereted image if exists
-            if (File.Exists(sFile) && previousColor == IconColor)
-            {
-                SetSource();
-                return;
-            }
-
-            // load svg
-            var svgText = (string)App.Current.Resources[index];
+            var svgText = (string)App.Current.Resources[SVGSource];
             var strm = new MemoryStream(Encoding.Default.GetBytes(svgText));
-            SVG.SKSvg svg = new SVG.SKSvg();
             svg.Load(strm);
+
+            var svgSize = svg.Picture.CullRect;
+            float svgMax = Math.Max(svgSize.Width, svgSize.Height);
 
             // get aspect ratio and responsive desing
             SKRect bounds = svg.Picture.CullRect;
-            float width = bounds.Width;
-            float height = bounds.Height;
-            double xRatio = parentWidth / width;
-            double yRatio = parentHeight / height;
-            float ratio = (float)Math.Min(xRatio, yRatio);
+            var padding = this.Parent.Padding();
+            float width = (float)(parentBounds.Width - padding.HorizontalThickness);
+            float height = (float)(parentBounds.Height - padding.VerticalThickness);
+            float xRatio = width / bounds.Width;
+            float yRatio = height / bounds.Height;
+            float ratio = Math.Min(xRatio, yRatio);
+
+            scale = SKMatrix.MakeScale(ratio, ratio);
 
             // generate a bitmap with a canvas
-            using (SKBitmap bitmap = new SKBitmap((int)(width), (int)(height)))
-            //using (SKBitmap bitmap = new SKBitmap((int)(width * ratio), (int)(height * ratio)))
-            using (SKCanvas canvas = new SKCanvas(bitmap))
+            bitmap = new SKBitmap((int)(bounds.Width * ratio), (int)(bounds.Height * ratio));
+            canvas = new SKCanvas(bitmap);
+#if DEBUG
+            Debug.WriteLine($"Load #{++loads}");
+#endif
+            return (svgLoaded = true).Value;
+        }
+
+        protected virtual void DrawImage()
+        {
+            svgLoading = true;
+
+            // load sgv bitmap and canvas
+            if (!LoadSVG())
             {
-                // resize canvas
-                //var matrix = SKMatrix.MakeScale(ratio, ratio);
-                //canvas.DrawPicture(svg.Picture, ref matrix);
-
-                // change color
-                if (iconColor is Color color)
-                {
-                    previousColor = iconColor;
-                    var paint = new SKPaint();
-                    paint.ColorFilter = SKColorFilter.CreateBlendMode(
-                        // the color, also `(SKColor)0xFFFF0000` is valid
-                        color.ToSKColor(),       
-                        SKBlendMode.SrcIn); // use the source color
-                    canvas.DrawPicture(svg.Picture, paint);
-                }
-                else canvas.DrawPicture(svg.Picture);
-                canvas.Flush();
-                canvas.Save();
-
-
-                // resize bitmap
-                SKBitmap resaizeBitmap = bitmap.Resize((new SKBitmap((int)(width * ratio), 
-                    (int)(height * ratio))).Info, SKFilterQuality.High);
-
-                // save generated bitmap
-                using (SKImage image = SKImage.FromBitmap(resaizeBitmap))
-                using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
-                using (MemoryStream memStream = new MemoryStream())
-                {
-                    data.SaveTo(memStream);
-                    memStream.Seek(0, SeekOrigin.Begin);
-
-                    FileStream fileStream = new FileStream(sFile, FileMode.Create);
-                    memStream.CopyTo(fileStream);
-                    fileStream.Close();
-                }
-                SetSource();
+                svgLoading = false;
+                return;
             }
 
-            void SetSource() => Source = ImageSource.FromStream(() => File.OpenRead(sFile));
+            // change color
+            canvas.Clear();
+            var scale = this.scale ?? SKMatrix.MakeScale(1, 1);
+            if (IconColor is Color color)
+            {
+                previousColor = color.ToSKColor();
+                var paint = new SKPaint();
+                paint.ColorFilter = SKColorFilter.CreateBlendMode(
+                    // the color, also `(SKColor)0xFFFF0000` is valid
+                    previousColor.Value,       
+                    SKBlendMode.SrcIn); // use the source color
+                canvas.DrawPicture(svg.Picture, ref scale, paint);
+            }
+            else canvas.DrawPicture(svg.Picture, ref scale);
+            canvas.Flush();
+            canvas.Save();
+
+            Device.BeginInvokeOnMainThread(async () => 
+            {
+                Source = ImageSource.FromStream(() =>
+                {
+                    SKImage image = SKImage.FromBitmap(bitmap);
+                    SKData data = image.Encode(SKEncodedImageFormat.Png, 100);
+                    return data.AsStream();
+                });
+                await Task.Delay(1000);
+                previousBounds = this.Parent.Bounds();
+                svgLoading = false;
+            });
+#if DEBUG
+            Debug.WriteLine($"Draw #{++draws}");
+#endif
         }
+
+        #region DEPRECATED
+
+        // load genereted image if exists DEPRECATED.
+        //xif (LoadSouce(SVGSource)) return;
+        //protected virtual bool LoadSouce(string fileName)
+        //{
+        //    string pathToFile = $"{FileSystem.AppDataDirectory}/{Path.GetFileNameWithoutExtension(fileName)}.png";
+        //    if (File.Exists(pathToFile) && previousColor == IconColor?.ToSKColor())
+        //    {
+        //        Source = ImageSource.FromStream(() => File.OpenRead(pathToFile));
+        //        return true;
+        //    }
+        //    else return false;
+        //}
+
+        // Save generated bitmap DEPRECATED.
+        //xSaveFile(resaizeBitmap, SVGSource);
+        //xLoadSouce(SVGSource);
+        //protected virtual void SaveFile(SKBitmap bitmap, string fileName)
+        //{
+        //    string pathToFile = $"{FileSystem.AppDataDirectory}/{Path.GetFileNameWithoutExtension(fileName)}.png";
+        //    // resize bitmap
+        //    SKBitmap resaizeBitmap = bitmap.Resize((new SKBitmap((int)(width * ratio), 
+        //        (int)(height * ratio))).Info, SKFilterQuality.High);
+        //    using (SKImage image = SKImage.FromBitmap(bitmap))
+        //    using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
+        //    using (Stream memStream = data.AsStream())
+        //    {
+        //        FileStream fileStream = new FileStream(pathToFile, FileMode.Create);
+        //        memStream.CopyTo(fileStream);
+        //        fileStream.Close();
+        //    }
+        //}
+
+        #endregion
 
         #endregion
 
@@ -170,12 +237,29 @@ namespace CheckListNotes.Pages.UserControls
 
         public void Dispose()
         {
+            this.MeasureInvalidated -= OnMeasureInvalidated;
             svgLoaded = null;
-            isDisposing = null;
+            svgLoading = null;
             previousColor = null;
+            previousBounds = null;
+            bitmap = null;
+            canvas = null;
+            scale = null;
+            if (canvas != null)
+            {
+                canvas.Dispose();
+                canvas = null;
+            }
+            if (bitmap != null)
+            {
+                bitmap.Dispose();
+                bitmap = null;
+            }
+            svg = null;
 #if DEBUG
             Debug.WriteLine("Object destroyect: [ Name: {0}, Id: {1} ].", nameof(SVGImaggeButton), this.GetHashCode());
 #endif
+            isDisposing = null;
         }
 
         #endregion
