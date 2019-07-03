559766d23decfcbed62ee583a738cd8f44f5a248
Jesse R. Jose <jessejose@outlook.es>
2019-07-03 00:43:40 -0400

    cambio de pociciones en debug

---
+++ CheckListNotes/CheckListNotes/PageModels/Converters/IntToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CheckListNotes.PageModels.Converters
+{
+    class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int toConvert = (int?)value ?? -1;
+            return toConvert.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string toConvert = (string)value ?? "0";
+            if (toConvert == "-" || string.IsNullOrEmpty(toConvert)) toConvert = "0";
+            bool sucess = int.TryParse(toConvert, out int resoult);
+            return sucess ? resoult : -1;
+        }
+    }
+}
--- CheckListNotes/CheckListNotes/Pages/TaskPage.xaml.cs
+++ CheckListNotes/CheckListNotes/Pages/TaskPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+using CheckListNotes.PageModels.Converters;
 
 namespace CheckListNotes.Pages
 {
@@ -12,6 +14,34 @@ namespace CheckListNotes.Pages
 
             NavigationPage.SetHasBackButton(this, false);
             NavigationPage.SetHasNavigationBar(this, false);
+
+            AddDebugControls();
         }
+
+        #region Debug Controls
+
+        [Conditional("DEBUG")]
+        private void AddDebugControls()
+        {
+            var label = new Label
+            {
+                Text = "Posición",
+                TextColor = (Color)App.Current.Resources["FontColor"]
+            };
+
+            var entry = new Entry
+            {
+                Placeholder = "Posicion",
+                Keyboard = Keyboard.Numeric,
+                TextColor = (Color)App.Current.Resources["FontColor"]
+            };
+
+            Container.Children.Add(label);
+            Container.Children.Add(entry);
+
+            entry.SetBinding(Entry.TextProperty, "Position", converter: new IntToStringConverter());
+        }
+
+        #endregion
     }
 }
